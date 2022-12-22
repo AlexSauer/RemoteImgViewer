72113ad7f59fcfa5f964575c43627418db2af1d8
Alex Sauer <sauer.alexander@gmx.de>
2022-12-22 12:08:08 +0100

    Simplified structure and first working example

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-function loadImage(imgPath) {
+function requestImage(imgPath, cur_index, cur_channel) {
     httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
@@ -17,51 +17,37 @@ function loadImage(imgPath) {
             }
         }
     }
-    httpRequest.open("POST", "api/setImage/");
+    httpRequest.open("POST", "api/requestImage/");
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send(`filePath=${encodeURIComponent(imgPath)}`); 
-}
-
-function viewImage(url) {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-        alert("Building Request failed!");
-        return false;
-    }
-
-    httpRequest.onreadystatechange = () => {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                let curImg = document.getElementById('myImage');
-                let response = httpRequest.responseText;
-                curImg.src = `data:imge/jpg;base64, ${response}`;
-            } else {
-                alert("Some problem with request!")
-            }
-        }
-    }
-    httpRequest.open("GET", url);
-    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send(); 
+    httpRequest.send(`filePath=${encodeURIComponent(imgPath)}&index=${cur_index}&channel=${cur_channel}`); 
 }
 
 window.onload = function() {
+    let CUR_INDEX = 0;
+    let CUR_CHANNEL = 2;
+    let CUR_PATH = ''
     const loadBotton = document.getElementById('loadButton');
     loadBotton.onclick = () => {
-        let imgPath = document.getElementById('filePathInput').value;
-        loadImage(imgPath);
+        CUR_INDEX = 0;
+        CUR_PATH = document.getElementById('filePathInput').value;
+        requestImage(CUR_PATH, CUR_INDEX, CUR_CHANNEL);
     }
 
     const prevButton = document.getElementById('prevButton');
     const nextButton = document.getElementById('nextButton');
+    const channelInput = document.getElementById('channelSelection');
 
     prevButton.onclick = () => {
-        viewImage('api/prevImage/')
+        CUR_INDEX = Math.max(0, CUR_INDEX -1);
+        requestImage(CUR_PATH, CUR_INDEX, CUR_CHANNEL);
     }
     nextButton.onclick = () => {
-        viewImage('api/nextImage/')
+        CUR_INDEX = CUR_INDEX+1;
+        requestImage(CUR_PATH, CUR_INDEX, CUR_CHANNEL)
     }
+
+
+
     
 
 }
